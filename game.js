a4c3961002e89630ec353c7896f3677666afe339
Alex <alexvelikiy@hotmail.com>
2024-02-29 18:02:40 -0500

    Completed the rock-paper-scissors game

--- game.js
+++ game.js
@@ -5,5 +5,43 @@ function getComputerChoice (){
     return choice[randomInt];
 }
 
-console.log(getComputerChoice());
+function playGame (playerSelection, computerSelection){
+    let playerChoice = playerSelection.toLowerCase();
+
+    if ((playerChoice === 'rock' && computerSelection === 'scissors') || (playerChoice === 'scissors' && computerSelection === 'paper') || (playerChoice === 'paper' && computerSelection === 'rock')){
+        console.log('Player wins!' + ' Player: ' + playerChoice + ' Computer: ' + computerSelection);
+        return 'player';
+    }else if(playerChoice === computerSelection){
+        console.log("It's a tie!" + ' Player: ' + playerChoice + ' Computer: ' + computerSelection);
+        return 'tie';
+    }else{
+        console.log('Computer won!' + ' Player: ' + playerChoice + ' Computer: ' + computerSelection);
+        return 'computer';
+    }
+}
+
+let userInput = null;
+let winner = null;
+let pScore = 0;
+let cScore = 0;
+
+for (let i = 0; i < 5; i++){
+    
+    userInput = prompt('Rock, Paper, or Scissors?');
+    winner = playGame(userInput, getComputerChoice());
+    
+    if (winner === 'computer'){
+        cScore += 1;
+    }else if (winner === 'player'){
+        pScore += 1;
+    }else{
+        cScore += 1;
+        pScore += 1;
+    }
+
+    console.log('------GAME ' + i+1 + ' RESULTS------------');
+    console.log('Player Score: ' + pScore);
+    console.log('Computer Score: ' + cScore);
+}
+
 
